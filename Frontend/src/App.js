51959d3035d1e8f3bfeb9bdb11325abb2bd080f0
Baptiste Keunebroek <baptiste.keunebroek@gmail.com>
2022-03-07 15:44:15 +0100

    css footer

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -10,39 +10,50 @@ import { Chat } from './components/Chat';
 
 function Home() {
   return (
-    <div>
-      <h1 className ="Presentation">Blind-Test</h1> 
-    <p className="Presentation2">Blind-Test est un site de quiz musicaux qui se veut simple d'utilisation et entièrement gratuit.
+    <div className="Presentation">
+      <h1 className="Presentation">Blind-Test</h1>
+      <p className="Presentation2">Blind-Test est un site de quiz musicaux qui se veut simple d'utilisation et entièrement gratuit.
 
-Vous n'avez pas besoin de vous inscrire pour lancer une partie.
+        Vous n'avez pas besoin de vous inscrire pour lancer une partie.
 
-Les inscriptions permettent de choisir un pseudonyme, de créer des playlists privées et d'apparaître dans le classement.</p>
-     
+        Les inscriptions permettent de choisir un pseudonyme, de créer des playlists privées et d'apparaître dans le classement.</p>
+
     </div>
   );
 }
 
-function NavigationBar(){
-  return(
-    <header>
-        <img className ="logo" src ="https://fr.seaicons.com/wp-content/uploads/2015/10/music-orange-icon.png" alt="logo" />
-        <nav>
-        <ul className ="liens_nav">
+function NavigationBar() {
+  return (
+    <div className="nav_bar">
+      <img className="logo" src="https://fr.seaicons.com/wp-content/uploads/2015/10/music-orange-icon.png" alt="logo" />
+      <nav>
+        <ul className="liens_nav">
           <li><Link to={'/'}>Home</Link></li>
           <li><Link to={'/join'}>Join</Link></li>
           <li><Link to={'/'}>Playlist</Link></li>
         </ul>
-        </nav>
-        <a className="contact" > <button>Contact</button></a>
-      </header>
-  ) 
+      </nav>
+      <a className="contact" > <button>Contact</button></a>
+    </div>
+
+  )
 }
 
+function Footer_(){
+return(
+  <div className="footer">
+        <div className="footer_text">
+          ceci est un test
+        </div>
+
+      </div>
+)
+}
 
 function App() {
   return (
     <div>
-      
+
       <Router>
         <NavigationBar />
         <Routes>
@@ -50,9 +61,11 @@ function App() {
           <Route path="/join" element={<Join />} />
           <Route path="/chat" element={<Chat />} />
         </Routes>
+        <Footer_ />
       </Router>
       
     </div>
+
   );
 }
 
