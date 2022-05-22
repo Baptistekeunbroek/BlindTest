0472a8c7deea54be883ec69a4fec5c08993505db
baptiste <baptiste.keunebroek@efrei.net>
2022-05-22 16:03:12 +0200

    contact

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,6 +2,12 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { Join } from "./components/Join";
 import { Chat } from "./components/Chat";
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardContent from '@mui/material/CardContent';
+import Box from '@mui/material/Box';
+import bk from './bk.jpeg';
 
 // Render a YouTube video player
 
@@ -50,10 +56,76 @@ function NavigationBar() {
       </nav>
       <div className="contact">
         {" "}
-        <button className="btn-75">Contact</button>
+        <button className="btn-75"><Link to={"/contact"}>Contact</Link></button>
       </div>
     </div>
   );
+}
+
+function Contact() {
+  return (
+    <Card sx={{display : 'flex', flexDirection:'row'}}>
+      <Box sx={{display:'flex', flexDirection:'column', border:'2px black'}}>
+      <CardMedia
+        component="img"
+        image={bk}
+        sx={{
+          height: 300,
+          width: 350,
+        }}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Baptiste Keunebroek
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          baptiste.keunebroek@efrei.net
+        </Typography>
+      </CardContent>
+      </Box>
+
+      <Box sx={{display:'flex', flexDirection:'column', border:'2px black'}}>
+      <CardMedia
+        component="img"
+        src="https://i1.sndcdn.com/avatars-O4Zf03w0rsVlYevf-E9twPw-t500x500.jpg"
+        sx={{
+          height: 300,
+          width: 350,
+        }}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Malo Le Corvec
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          adresse test info
+        </Typography>
+      </CardContent>
+      </Box>
+
+      <Box sx={{display:'flex', flexDirection:'column', border:'2px black'}}>
+      <CardMedia
+        component="img"
+        src="https://i1.sndcdn.com/avatars-O4Zf03w0rsVlYevf-E9twPw-t500x500.jpg"
+        sx={{
+          height: 300,
+          width: 350,
+        }}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Malo Le Corvec
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          adresse test info
+        </Typography>
+      </CardContent>
+      </Box>
+      
+    </Card>
+
+
+  )
 }
 
 // function Footer_() {
@@ -73,6 +145,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/join" element={<Join />} />
           <Route path="/chat" element={<Chat />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
