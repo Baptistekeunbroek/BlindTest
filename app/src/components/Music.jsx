5db1c11a558a9c31f11837c25ab5148fea05934d
Malo Le Corvec <malolc2001@gmail.com>
2023-05-09 15:27:30 +0200

    Update Music.jsx

--- app/src/components/Music.jsx
+++ app/src/components/Music.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Music.css";
 
 export function Music({ YTurl, socket }) {
@@ -6,16 +5,16 @@ export function Music({ YTurl, socket }) {
     socket.emit("readyToPlay");
   }
 
-  if (!YTurl) return <div></div>;
+  if (!YTurl) return null;
 
   return (
     <div className="Music">
       <iframe
         onLoad={pret}
-        onError={() => socket.emit("putUrl")}
         className="iframe"
-        src={`https://yewtu.be/embed/${YTurl}?iv_load_policy=3&autoplay=1&continue=0&listen=true&quality=medium&comments=false&loop=0&volume=20&controls=0`}
+        src={`https://yewtu.be/embed/${YTurl}?local=true&iv_load_policy=3&autoplay=1&continue=0&listen=true&quality=medium&related_videos=false&comments=false&loop=0&volume=20&controls=0`}
         title="YouTube video player"
+        allow="autoplay"
       />
     </div>
   );
