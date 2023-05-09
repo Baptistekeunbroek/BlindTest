ea0591a71ac2b18da6db67b6be3a772034645ea3
Malo Le Corvec <malolc2001@gmail.com>
2023-05-09 15:40:18 +0200

    contact

--- app/src/components/Contact.jsx
+++ app/src/components/Contact.jsx
@@ -7,39 +7,39 @@ import "./Contact.css";
 
 export function Contact() {
   return (
-    <div>
-      <div>
-        <img src={malo} />
+    <div className="contactBig">
+      <div className="contactUser">
+        <img className="contactImg" src={malo} />
         <div>
           <div>Malo Le Corvec</div>
           <div>malo.le.corvec@efrei.net</div>
         </div>
       </div>
-      <div>
-        <img src={bk} />
+      <div className="contactUser">
+        <img className="contactImg" src={bk} />
         <div>
           <div>Baptiste Keunebroek</div>
           <div>baptiste.keunebroek@efrei.net</div>
         </div>
       </div>
 
-      <div>
-        <img src={clement} />
+      <div className="contactUser">
+        <img className="contactImg" src={clement} />
         <div>
           <div>Clément Le Corre</div>
           <div>clement.le.corre@efrei.net</div>
         </div>
       </div>
 
-      <div>
-        <img src={luk} />
+      <div className="contactUser">
+        <img className="contactImg" src={luk} />
         <div>
           <div>Lucas Le Page</div>
           <div>lucas.le.page@efrei.net</div>
         </div>
       </div>
-      <div>
-        <img src={rems} />
+      <div className="contactUser">
+        <img className="contactImg" src={rems} />
         <div>
           <div>Remi Paucton</div>
           <div>remi.paucton@efrei.net</div>
