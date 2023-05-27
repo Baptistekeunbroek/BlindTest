c0d2a86e00b1bf092745d3001f88f90febeca02f
Baptiste Keunebroek <baptiste.keunebroek@efrei.net>
2023-05-27 03:44:22 +0200

    fix ui et add limite text

--- app/src/components/Join.jsx
+++ app/src/components/Join.jsx
@@ -18,22 +18,26 @@ export function Join() {
       <NavLink to="https://lejeudelaplaylist.onrender.com/" className="absolute" end>
         <HiArrowLeft className="text-white ml-2 mt-2 w-10 h-10 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" alt="icone fleche retour" />
       </NavLink>
-      <div className="w-full h-full flex flex-col items-center justify-center">
-        <div className="justify-center flex flex-col gap-4 items-center">
-          <h1 className="text-lg my-10 text-white font-semibold ">Le Blind-Test</h1>
+      <div className="w-full h-full flex flex-col items-center justify-center ">
+        <div className="justify-center flex flex-col gap-4 items-center bg-[#242531] rounded-xl mt-8 w-96 ">
+          <h1 className="text-lg mt-2 text-white font-semibold ">Le Blind-Test</h1>
           <input
             placeholder="Nom de la room"
             type="text"
-            className="bg-[#242531] !ring-0 !outline-none rounded-3xl text-white shadow-md shadow-[rgba(0, 0, 0, 0.25)] p-6 py-3 font-semibold "
+            required
+            maxLength={15}
+            className="bg-[#242531] !ring-0 !outline-none rounded-3xl text-white shadow-md shadow-[rgba(0, 0, 0, 0.25)] p-6 py-3 font-semibold shadow-[#00FECC] "
             ref={roomRef}
           />
           <input
             placeholder="Ton Pseudo"
-            className="bg-[#242531] !ring-0 !outline-none rounded-3xl text-white shadow-md shadow-[rgba(0, 0, 0, 0.25)] p-6 py-3 font-semibold "
+            required
+            maxLength={15}
+            className="bg-[#242531] !ring-0 !outline-none rounded-3xl text-white shadow-md shadow-[rgba(0, 0, 0, 0.25)] p-6 py-3 font-semibold shadow-[#00FECC] "
             type="text"
             ref={nameRef}
           />
-          <button onClick={join} type="submit" className="bg-[#FDFDFD] rounded-3xl text-center flex flex-row justify-center items-center mt-4 p-2 font-semibold">
+          <button onClick={join} type="submit" className="bg-[#FDFDFD] rounded-3xl text-center flex flex-row justify-center items-center mb-4 mt-4 p-2 font-semibold">
             Rejoindre
           </button>
         </div>
