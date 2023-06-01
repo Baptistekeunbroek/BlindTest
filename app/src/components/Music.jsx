e7fb4c2e52b8a09f47b80c724fe7c982718175c6
Malo Le Corvec <malolc2001@gmail.com>
2023-06-01 17:13:47 +0200

    Update Music.jsx

--- app/src/components/Music.jsx
+++ app/src/components/Music.jsx
@@ -1,20 +1,34 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, createElement } from "react";
 
 export function Music({ YTurl, socket }) {
   const [volume, setVolume] = useState(localStorage.getItem("volume") || 20);
+  const [IframeComponent, setIframeComponent] = useState(null);
 
   useEffect(() => {
     if (!YTurl) return;
-    // little hack to prevent the iframe from appearing in the browser history
-    const ifr = document.getElementById("ytplayer");
-    ifr.contentWindow.location.replace(
-      `https://yewtu.be/embed/${YTurl}?local=true&iv_load_policy=3&autoplay=1&continue=0&listen=true&quality=medium&related_videos=false&comments=false&loop=0&volume=${volume}`,
+    // hack change iframe src without adding history entry
+    setIframeComponent(
+      // eslint-disable-next-line react/display-name
+      () => () =>
+        createElement(
+          "iframe",
+          {
+            src: `https://yewtu.be/embed/${YTurl}?local=true&iv_load_policy=3&autoplay=1&continue=0&listen=true&quality=medium&related_videos=false&comments=false&loop=0&volume=${volume}`,
+            className: "hidden",
+            id: "ytplayer",
+            name: "youtube-player",
+            onLoad: () => socket.emit("readyToPlay"),
+            title: "YouTube video player",
+            allow: "autoplay",
+          },
+          null,
+        ),
     );
-    if (import.meta.env.DEV) console.log("iframe loaded");
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [YTurl]);
 
   if (!YTurl) return null;
+  if (!IframeComponent) return null;
 
   return (
     <div className="flex flex-col items-center">
@@ -30,7 +44,7 @@ export function Music({ YTurl, socket }) {
         className="w-56 mt-10"
       />
       <div className="text-white text-xs">Le changement de volume est pris en compte au prochain titre</div>
-      <iframe className="hidden" id="ytplayer" name="youtube-player" onLoad={() => socket.emit("readyToPlay")} title="YouTube video player" allow="autoplay" />
+      <IframeComponent />
     </div>
   );
 }
